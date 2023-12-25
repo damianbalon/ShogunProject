c0f222ad986013edd963e5dab973162a7553a325
Marvee2202 <marcin5220222@gmail.com>
2023-12-25 21:00:50 +0100

    Dodano szkielet mapy taktycznej; funkcjonalność coming soon

---
+++ ShogunProjectU/Assets/Scripts/Grid/TacticalMap.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TacticalMap : MonoBehaviour
+{
+    [SerializeField]
+    private TacticalTile tilePrefab;
+    public GameObject tileContainer;
+    private Dictionary<Vector2Int, TacticalTile> map;
+    void Awake() {
+        var tileMap = gameObject.GetComponentInChildren<Tilemap>();
+        BoundsInt bounds = tileMap.cellBounds;
+        map = new Dictionary<Vector2Int, TacticalTile>();
+
+        //cycle through all tilemap cells and generate an interactive grid
+        for(int z = bounds.max.z; z >= bounds.min.z; z--) //we want the highest-z tile to be playable as it is not buried under the others
+        {
+            for(int y = bounds.min.y; y <= bounds.max.y; y++)
+            {
+                for( int x = bounds.min.x; x <= bounds.max.x; x++)
+                {
+                    var tileLocation = new Vector3Int(x,y,z);
+                    var tileKey = new Vector2Int(x,y);
+
+                    if(tileMap.HasTile(tileLocation) && !map.ContainsKey(tileKey))
+                    {
+                        var overlayTile = Instantiate(tilePrefab, tileContainer.transform);
+                        var cellWorldPosition = tileMap.GetCellCenterWorld(tileLocation);
+                        overlayTile.transform.position = new Vector3(cellWorldPosition.x,cellWorldPosition.y,cellWorldPosition.z+0.6f);
+                        overlayTile.GetComponent<SpriteRenderer>().sortingOrder = tileMap.GetComponent<TilemapRenderer>().sortingOrder;
+                        overlayTile.GridLocation = tileLocation;
+                        map.Add(tileKey, overlayTile);
+                    }
+                }
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDestroy() {
+        foreach(var i in map) {
+            Destroy(i.Value.gameObject);
+            map.Remove(i.Key);
+        }
+    }
+}
---
+++ ShogunProjectU/Assets/Scripts/Grid/TacticalTile.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TacticalTile : MonoBehaviour
+{
+    private Vector3Int gridLocation;
+    public Vector3Int GridLocation {
+        get {return gridLocation;}
+        set {gridLocation = value;}
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
