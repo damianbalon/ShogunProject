4f1b8011b28a45f1a43d07ef318eedc80405d505
Marvee2202 <marcin5220222@gmail.com>
2023-12-27 17:17:44 +0100

    Aktualizacja sceny

--- ShogunProjectU/Assets/Scripts/CharacterInitializer.cs
+++ ShogunProjectU/Assets/Scripts/CharacterInitializer.cs
@@ -16,7 +16,7 @@ public class CharacterInitializer : MonoBehaviour//this class automatically alig
     void FixedUpdate()
     {
         if(character.OccupiedTile != null) Destroy(this);
-        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position,Vector2.zero);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position,Vector2.zero, Mathf.Infinity);
         if(hits.Length > 0)
         {
             System.Array.Sort(hits, (x, y) => x.distance.CompareTo(y.distance));
