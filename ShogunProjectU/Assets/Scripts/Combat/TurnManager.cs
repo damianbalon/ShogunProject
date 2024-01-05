d377192d235d3ab8a63d3439232ac43c95c7f08e
Marvee2202 <marcin5220222@gmail.com>
2024-01-05 12:03:04 +0100

    Podstawowa obsługa kolejki akcji

--- ShogunProjectU/Assets/Scripts/Combat/TurnManager.cs
+++ ShogunProjectU/Assets/Scripts/Combat/TurnManager.cs
@@ -6,14 +6,14 @@ using UnityEngine;
 public class TurnManager : MonoBehaviour
 {
     [SerializeField] private GameEvent endTurnEvent;
-    //[SerializeField] private GameEvent endActionEvent; może to będzie odbierał gdy postać skończy turę a nie samemu wysyłał
+    [SerializeField] private GameEvent nextActionEvent;
     private List<CombatBehavior> combatants;
     private List<Tuple<CombatBehavior, int>> turnOrder;
-    private CombatBehavior currentActor;
+    private GameObject currentActor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        turnOrder = new List<Tuple<CombatBehavior, int>>();
     }
 
     // Update is called once per frame
@@ -22,5 +22,21 @@ public class TurnManager : MonoBehaviour
         
     }
 
+    void ConstructTurnOrder() {
+        turnOrder.Clear();
+        foreach(var item in combatants) {
+            turnOrder.Add(new Tuple<CombatBehavior, int>(item, item.Initiative));
+        }
+        turnOrder.Sort((x, y) => x.Item2.CompareTo(y.Item2));
+        currentActor = turnOrder[turnOrder.Count - 1].Item1.gameObject;
+    }
 
+    void NextAction() {
+        turnOrder.RemoveAt(turnOrder.Count - 1);
+        if(turnOrder.Count == 0) {
+            endTurnEvent.Raise();
+            ConstructTurnOrder();
+        }
+        else currentActor = turnOrder[turnOrder.Count - 1].Item1.gameObject;
+    }
 }
